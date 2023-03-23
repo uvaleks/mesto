7e626ddac47db8232525378bf5f4bfd0f982c271
uvaleks <uvaleks@protonmail.com>
2023-03-24 00:44:22 +0300

    hotfix: fix 1st review bugs

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
-# Проект: Место
+# Проект: Mesto Russia
 
-### Обзор
+Учебный проект Яндекс Практикума, выполняемый на третьем месяце обучения веб-разработке (4 спринт)
 
-* Figma
-* Картинки
+## Используемые технологии:
+* HTML
+* CSS
+* JavaScript
+* Git
 
-**Figma**
+## Используемые инстументы:
+* Figma. [Ссылка на макет](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+* [Сервис по масштабированию png](https://www.iloveimg.com/resize-image/resize-png)
+* [Сервис по оптимизации png](tinypng.com)
+* Visual Studio Code
+* GitHub
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+## Планы по доработке
+* Реализовать смену языка страницы по кнопке в шапке
 
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+* [Ссылка на GitHub Pages](https://uvaleks.github.io/mesto/)
--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,18 @@ let editButton = document.querySelector('.profile__edit-button')
 let profileName = document.querySelector('.profile__profile-name')
 let profileJob = document.querySelector('.profile__profile-description')
 let formElement = document.querySelector('.popup')
-let nameInput = formElement.querySelector('.popup__input-name')
-let jobInput = formElement.querySelector('.popup__input-description')
+let nameInput = formElement.querySelector('.popup__input_content_name')
+let jobInput = formElement.querySelector('.popup__input_content_description')
 let closeButton = formElement.querySelector('.popup__close-button')
 
 function togglePopup () {
-        formElement.classList.toggle('popup_opened')
+    if (formElement.classList.contains("popup_opened") === false) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    formElement.classList.toggle('popup_opened');
+    } else {
+    formElement.classList.toggle('popup_opened');
+    }
 }
 
 editButton.addEventListener('click', togglePopup); 
@@ -16,9 +22,8 @@ closeButton.addEventListener('click', togglePopup);
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
-    nameInput.placeholder = nameInput.value;
     profileJob.textContent = jobInput.value;
-    jobInput.placeholder = jobInput.value;
+    togglePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
